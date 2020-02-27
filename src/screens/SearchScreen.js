e7b45a84a3562400efd61928fd040adb0bbf53ea
Gabriel Gonzaga <gabrielmgonzaga@gmail.com>
2020-02-27 09:57:28 -0800

    add yelp search functionality

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,31 @@
 import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import SearchBar from '../components/SearchBar'
+import yelp from '../api/yelp'
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('')
+    const [results, setResults] = useState([])
+
+    const searchApi = async () => {
+        const response = await yelp.get('/search', {
+            params: {
+                term,
+                limit: 50,
+                location: 'irvine'
+            }
+        })
+        setResults(response.data.businesses)
+    }
 
     return (
         <View>
             <SearchBar 
                 term={term} 
                 onTermChange={newTerm => setTerm(newTerm)} 
-                onTermSubmit={() => console.log('Submitted')}
+                onTermSubmit={() => searchApi()}
             />
-            <Text>{term}</Text>
+            <Text>We have found {results.length}</Text>
         </View>
     )
 }
